ac7976a393b33ea63a72fe0fc121a9fc5c4e3356
abodh5921 <abodh.shah@surepass.io>
2024-09-21 19:31:43 +0530

    Episode 1 Inception Code

---
+++ App.js
@@ -0,0 +1,26 @@
+const heading = React.createElement(
+  'h1',
+  { id: 'heading' },
+  'Hello World From React!'
+)
+// console.log(heading)
+
+const parent = React.createElement(
+  'div',
+  { id: "parent" },
+  [React.createElement(
+    'div',
+    { id: "child1" },
+    [React.createElement('h1', {}, "I'm a h1 tag"),React.createElement('h2', {}, "I'm a h2 tag")]
+  ),
+  React.createElement(
+    'div',
+    { id: "child2" },
+    [React.createElement('h1', {}, "I'm a h1 tag"),React.createElement('h2', {}, "I'm a h2 tag")]
+  )]
+);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(parent)
