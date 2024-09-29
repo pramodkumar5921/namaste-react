06f874caf90403cda48af3bae07adafeca8456ef
abodh5921 <abodh.shah@surepass.io>
2024-09-29 11:09:32 +0530

    Api is Working Fine Without Any Error

--- src/compontents/Body.js
+++ src/compontents/Body.js
@@ -1,36 +1,77 @@
 import RestaurantCard from "./RestaurentCard";
-import resList from "../utils/MockData";
-import { useState } from "react";
-const Body = ()=>{
-  /**
-   * Use State Variable - Super power Varible
-   */
-  const arr = useState(resList);
-  // const [Listrestaurent,setListrestaurent] = arr;
-  const Listrestaurent = arr[0];
-  const setListrestaurent = arr[1];
-    return (
-      <div className="body">
-       <div className="filter">
-        <button className="filter-btn" 
-        onClick={()=>{
-          const filterList = Listrestaurent.filter(
-            (res) => res.restaurant.data.rating > 4
-          );
-          // console.log(Listrestaurent);
-          console.log("filterList......",filterList)
-          setListrestaurent(filterList);
-        }}
-        >Top Rated Restaurant</button>
-       </div>
-       <div className="res-container">
-        {Listrestaurent.map((restaurantItem) => (
-          <RestaurantCard key = {restaurantItem?.restaurant?.data?.id} resData = {restaurantItem}/>
-    
-        ))}
-       </div>
-      </div>
+import { Api_URL } from "../utils/constants";
+import { useEffect, useState } from "react";
+
+
+const Body = () => {
+  const [Listrestaurent, setListrestaurent] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+     Api_URL
     );
+    const json = await data.json();
+    setListrestaurent(json?.data?.cards);
   };
 
-  export default Body;
+  // const fetchData = async () => {
+  //   const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6624624&lng=77.0722759&collection=83637&tags=layout_CCS_Burger&sortBy=&filters=&type=rcv2&offset=0&page_type=null");
+  //   const json = await data.json();
+
+  //   console.log(json.data.cards[3].card.card.info.name);
+  //   console.log(json.data.cards[4].card.card.info.name);
+  //   console.log(json.data.cards[5].card.card.info.name);
+  //   console.log(json.data.cards[6].card.card.info.name);
+  //   console.log(json.data.cards[7].card.card.info.name);
+  //   console.log(json.data.cards[8].card.card.info.name);
+  //   console.log(json.data.cards[9].card.card.info.name);
+  // };
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filterList = [];
+            // const filterList = Listrestaurent.filter(
+            //   (res) => res.restaurant.data.rating > 4
+            // );
+            // setListrestaurent(filterList);
+            for(let i=3;i<Listrestaurent.length;i++){
+              const restaurantItem = Listrestaurent[i];
+             if(restaurantItem?.card?.card?.info?.avgRating > 4 ){
+                filterList.push(restaurantItem);
+             }
+            }
+            setListrestaurent(filterList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+      <div className="res-container">
+        {/* Convert the map function to for loop */}
+        {(() => {
+          const restaurantCards = [];
+          for (let i = 3; i < Listrestaurent.length; i++) {
+            const restaurantItem = Listrestaurent[i];
+            restaurantCards.push(
+              <RestaurantCard
+                key={restaurantItem?.card?.card?.info?.id}
+                resData={restaurantItem}
+              />
+            );
+          }
+          return restaurantCards;
+        })()}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
