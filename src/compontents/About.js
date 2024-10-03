0d8752649fa827f29b882c2be6bf53997e331e73
abodh5921 <abodh.shah@surepass.io>
2024-10-03 11:08:35 +0530

    Episode-08 is Completed

--- src/compontents/About.js
+++ src/compontents/About.js
@@ -1,9 +1,20 @@
-const About = ()=>{
-    return (
-        <div>
-            <h1>About</h1>
-            <h2>This is Namaste React Web Series</h2>
-        </div>
-    );
+import UserClass from "./UserClass.js";
+import React  from "react";
+
+class About extends React.Component{
+    constructor(props){
+        super(props);
+        console.log("parent class");
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>About</h1>
+                <h2>This is Namaste React Web Series</h2>
+                <UserClass name = {"Pramod Kumar"} location = {"Bihar"} />
+            </div>
+        );
+    }
 };
 export default About;
---
+++ src/compontents/UserClass.js
@@ -0,0 +1,36 @@
+import React from 'react'
+class UserClass extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      userInfo: {
+        name: 'Dummy',
+        location: 'Default',
+        avatar_url : "https/photo-dummy"
+      }
+    }
+  }
+
+  async componentDidMount () {
+    const data = await fetch('https://api.github.com/users/pramodkumar5921');
+    const json = await data.json();
+    this.setState({
+      userInfo: json
+    })
+  }
+
+
+  render () {
+    const {name,location ,  avatar_url } = this.state.userInfo;
+    return (
+      <div className='user-card'>
+        <img src = { avatar_url } />
+        <h2> Name : {name}</h2>
+        <h3>Location : {location}</h3>
+        <h4>Contact : @pramodkumar</h4>
+      </div>
+    )
+  }
+}
+
+export default UserClass
